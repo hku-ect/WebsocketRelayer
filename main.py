1dcc806affe0f22117e07139c7698ea405784bfd
Arnaud Loonstra <arnaud@sphaero.org>
2020-05-26 11:37:49 +0200

    simplify server code, tested with touchdesigner

--- main.py
+++ main.py
@@ -6,35 +6,38 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
-bla = 10
+# track number of clients connected (currently including conductor)
+clients = 0
 
 def messageReceived(methods=['GET', 'POST']):
-	print('message was received!!!')
+    print('message was received!!!')
 
 @app.route('/')
 def sessions():
     return render_template('session.html')
 
+@app.route('/conductor')
+def conductor():
+    return render_template('conductor.html')
+
 @socketio.on('connect')
-def test_connect():
-    socketio.emit('registration', {'event': 'Connected', 'clientid': request.sid })
+def on_connect():
+    print("Client connect event")
+    global clients
+    clients += 1
+    socketio.emit('connect', {'event': 'Connected', 'clientid': request.sid });
 
 @socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
+def on_disconnect():
+    global clients
+    clients -= 1
+    socketio.emit('disconnect', {'event': 'Disconnected', 'clientid': request.sid });
+    print( 'Client disconnected', request.sid )
 
 @socketio.on('message')
-def handle_message(msg):
-    global bla
-    bla += 1
+def on_message(msg):
     print('received message: {}'.format(msg))
-    msg['bla'] = bla
-    socketio.emit("bla", msg);
-
-@socketio.on('registration')
-def handle_json(msg):
-    print('received data: {}'.format( msg ))
-    socketio.emit("registration", msg);
+    socketio.emit("message", { 'clientid': request.sid, 'message': msg });
 
 @socketio.on_error_default
 def default_error_handler(e):
@@ -43,6 +46,7 @@ def default_error_handler(e):
     print(request.event["message"]) # "my error event"
     print(request.event["args"])    # (data,)
     print("######### END ERROR HANDLER #####")
+
 
 if __name__ == '__main__':
     socketio.run(app, host= '0.0.0.0', debug=True)
